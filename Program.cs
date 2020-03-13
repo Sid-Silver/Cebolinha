fcebce43901217d67f7870f9d71d9fdb755472b8
Guilherme (Sid) <60995375+Sid-Silver@users.noreply.github.com>
2020-03-13 20:39:44 -0300

    Adicionando a lição "cebolinha"

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite uma frase:");
+            string frase;
+            frase = Console.ReadLine();
+
+            Console.WriteLine("Cebolinha Diz:");
+
+            string Cebolinha;
+            Cebolinha = frase.Replace("r", "l").Replace("R","L");
+            Console.WriteLine(Cebolinha);
+            
+
+           
+        }
+    }
+}
